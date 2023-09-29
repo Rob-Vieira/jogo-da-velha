68c415430d24e00081d7601d0d35429fbe1edbee
Robson Vieira <robsonvieira97@gmail.com>
2023-09-29 12:07:24 -0300

    Tela inicial

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let o_icons = document.querySelectorAll('.board .space .o-icon');
-
-    console.log(o_icons);
-    console.log('e');
-
-    o_icons.forEach(e => e.classList.add('o-icon-transition'));
+    
 });
